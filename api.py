b850279c70f5523521f6a6f5429ae1948a922fb3
Yosef <yosefnoorani@gmail.com>
2024-04-15 23:23:44 +0300

    delete commenet

--- api.py
+++ api.py
@@ -1,17 +1,12 @@
 import os
-# import textwrap
-
-# import markdown
 from PIL import Image
 import google.generativeai as genai
 from flask import Flask, request, jsonify
-# from IPython.display import Markdown
 
 app = Flask(__name__)
 
 
 def generate_content(image_path):
-    #test
     GOOGLE_API_KEY = 'AIzaSyAPl3if3Qhr5i1dmSLD_RVyZT_p9nyTneM'
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel('gemini-pro-vision')
@@ -19,19 +14,11 @@ def generate_content(image_path):
     img = Image.open(image_path)
     content = "I am looking for such a product. Can you provide me 1. What is the name of the manufacturer of the product (Only the name)2. What is the name of the product 3. Describe and expand knowledge about the product 4. Give me a technical specification about the product 5. Offer me cheaper similar products with prices 6. URL to the store to purchase the original product"
     response = model.generate_content([
-                                        # ,
-                                          # "Describe the item on the picture and where I can buy it and give me other suggestions for similar products at a lower price",
-                                          # "give me other suggestions for similar products at a lower price",
-                                        content,
+                                          content,
                                           img], stream=True)
     response.resolve()
     print(model.count_tokens(response.text))
     return response.text
-
-
-# def to_markdown(text):
-#     text = text.replace('•', '  *')
-#     return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 
 @app.route('/', methods=['GET'])
@@ -45,7 +32,7 @@ def generate_from_image():
         return jsonify({'error': 'No image provided'})
 
     image = request.files['image']
-    print(image)
+    # print(image)
     if image.filename == '':
         return jsonify({'error': 'No selected image file'})
 
@@ -56,23 +43,9 @@ def generate_from_image():
     # Generate content from the image
     generated_text = generate_content(temp_image_path)
     # generated_text = generate_content(image)
-    print(generated_text)
+    # print(generated_text)
     os.remove(temp_image_path)
     return generated_text
-
-    # Delete the temporary image file
-    # os.remove(temp_image_path)
-
-    # markdown_text = to_markdown(generated_text)
-
-    # Convert Markdown to HTML
-    # html_content = markdown.markdown(generated_text)
-
-    # Delete the temporary image file
-
-    # return html_content
-
-    # return markdown_text
 
 
 if __name__ == '__main__':
