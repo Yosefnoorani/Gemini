020ce8c52669aff87125e7cf1fe51855e728c8fb
Yosef <yosefnoorani@gmail.com>
2024-04-21 02:12:35 +0300

    delete the key from main

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 #
 # # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
-#     GOOGLE_API_KEY = '@@@@@@@'
+#     GOOGLE_API_KEY = '@@@@@@@@@'
 #
 #     genai.configure(api_key=GOOGLE_API_KEY)
 #
