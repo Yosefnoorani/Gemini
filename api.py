e0864505281eacb860551802c7f935677b26d9ea
Yosef <yosefnoorani@gmail.com>
2024-04-24 16:16:49 +0300

    add result and remove bug in the json V6

--- api.py
+++ api.py
@@ -10,9 +10,8 @@ app = Flask(__name__)
 CORS(app)
 
 def generate_content(image_path):
-    # google_api_key = os.environ.get('GOOGLE_API_KEY')
+    google_api_key = os.environ.get('GOOGLE_API_KEY')
 
-    google_api_key = 'AIzaSyAPl3if3Qhr5i1dmSLD_RVyZT_p9nyTneM'
 
     genai.configure(api_key=google_api_key)
     model = genai.GenerativeModel('gemini-pro-vision')
